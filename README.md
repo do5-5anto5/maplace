5d64393cc19e06446fe4378faafe088ffc260198
Jonathan <dos.santos.2k23@gmail.com>
2025-07-08 14:08:30 -0300

    Start project: - lib package   - home.dart   - main.dart   - theme.dart - pubspec - README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# MaPlace
+
+## Getting Started
+
+This Mobile App made with Dart/Flutter saves your favorite places in a local database.
+
+### As main features:
+- Local Storage
+- Camera usage
+- Location
+
+### Third-party Dependencies
+- Google Fonts
+- UUID
+- Riverpod
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:maplace/utils/theme.dart';
+import 'package:maplace/widets/home.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Great Places',
+      theme: theme,
+      home: Home(),
+    );
+  }
+}
