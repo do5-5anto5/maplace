ce6cbf4cc36a5784f201ac63db61c40290fe0fe4
Jonathan <dos.santos.2k23@gmail.com>
2025-07-09 11:51:17 -0300

    feat: Implement userPlacesProvider in providers/user_places.dart

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:maplace/models/place.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super([]);
+
+  void addPlace(String title) {
+    state = [...state, Place(title: title)];
+  }
+
+  void removePlace(String id) {
+    state = state.where((place) => place.id != id).toList();
+  }
+}
+
+final userPlacesProvider =
+    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+      (ref) => UserPlacesNotifier(),
+    );
