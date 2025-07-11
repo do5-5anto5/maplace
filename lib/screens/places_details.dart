0092549c9e421c69b860aeac0b7990335a53491b
Jonathan <dos.santos.2k23@gmail.com>
2025-07-11 09:51:29 -0300

    feature: Output rendering a map with address on PlacesDetailsScreen

--- lib/screens/places_details.dart
+++ lib/screens/places_details.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:maplace/models/place.dart';
+import 'package:maplace/utils/location_image_url.dart';
 
 class PlaceDetailsScreen extends ConsumerWidget {
   const PlaceDetailsScreen(this.place, {super.key});
@@ -9,19 +9,56 @@ class PlaceDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(appBar: AppBar (
-      title: Text(place.title),
-    ),
+    final address = place.location?.address ?? '';
+    final locationImage = LocationImageUrl().locationImage(place.location);
+
+    return Scaffold(
+      appBar: AppBar(title: Text(place.title)),
       body: Stack(
         children: [
           Image.file(
-           place.image,
+            place.image,
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
-          )
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(locationImage),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 16,
+                  ),
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Colors.transparent,
+                      Colors.black54,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    ),
+                  ),
+                  child: Text(
+                    address,
+                    style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
