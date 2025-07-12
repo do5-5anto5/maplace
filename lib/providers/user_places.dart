69c4481942d798bff62433647d6e79c86e8ce96f
Jonathan <dos.santos.2k23@gmail.com>
2025-07-12 20:22:22 -0300

    feat: Storing place data in a local SQL database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite_common/sqlite_api.dart';
 
 import 'package:maplace/models/place.dart';
 
@@ -18,6 +20,26 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       image: copiedImage,
       location: location,
     );
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+        );
+      },
+        version: 1
+    );
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image,
+      'lat': newPlace.location!.latitude,
+      'lng': newPlace.location!.longitude,
+      'address': newPlace.location!.address,
+
+    });
 
     state = [...state, newPlace];
   }
