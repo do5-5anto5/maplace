a7e59eb0146e61405ad6aec36bad7cb42bd8988c
Jonathan <dos.santos.2k23@gmail.com>
2025-07-12 20:44:37 -0300

    feat: Conditionally auto create (or not) an id to Place object when instantiate. - In cause of parsing an object from database, it will come with an id

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -5,7 +5,8 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 
 class Place {
-  Place( {required this.image, required this.title, required this.location}) : id = uuid.v4();
+  Place({required this.image, required this.title, required this.location, String? id})
+    : id = id ?? uuid.v4();
 
   final String id;
   final String title;
